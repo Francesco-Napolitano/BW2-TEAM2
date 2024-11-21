a66f344e2ec42de19a162824b762feb39613124e
Massimo Pizzicannella <maxpizzi2605@gmail.com>
2024-11-21 01:09:46 +0100

    commit aggiunta brani, info artista e dettaglini con js

--- assets/JS/album-page.js
+++ assets/JS/album-page.js
@@ -1,61 +1,113 @@
-// const data = () => {
-//   fetch('https://striveschool-api.herokuapp.com/api/deezer/album/75621062')
-//     .then((response) => {
-//       if (response.ok) {
-//         return response.json()
-//       } else {
-//         throw new Error("Errore nel recupero dell'album")
-//       }
-//     })
-//     .then((album) => {
-//       console.log(album)
-//       const titleContain = document.getElementById('title')
+const data = () => {
+  fetch('https://striveschool-api.herokuapp.com/api/deezer/album/71437082')
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error("Errore nel recupero dell'album")
+      }
+    })
+    .then((album) => {
+      console.log(album)
+      const titleContain = document.getElementById('title')
 
-//       const title = document.createElement('h1')
-//       title.innerText = album.title
+      const title = document.createElement('h1')
+      title.innerText = album.title
 
-//       titleContain.appendChild(title)
-//       const imgContain = document.getElementById('cover')
-//       imgContain.innerHTML = ` <img
-//                      src="${album.cover_medium}"
-//                     class="img-fluid w-75 rounded shadowCover"
-//                     alt="Album Cover"
-//                   />`
+      titleContain.appendChild(title)
 
-//       function duration() {
-//         const hours = Math.floor(album.duration / 3600)
-//         const minutes = Math.floor((album.duration % 3600) / 60)
-//         const seconds = album.duration % 60
+      const singer = document.getElementById('singer')
+      singer.setAttribute('src', album.artist.picture)
 
-//         return `${hours}h ${minutes}m ${seconds}s`
-//       }
+      const cover = document.getElementById('cover')
+      cover.setAttribute('src', album.cover_medium)
 
-//       album.duration = duration()
+      function duration() {
+        const hours = Math.floor(album.duration / 3600)
+        const minutes = Math.floor((album.duration % 3600) / 60)
+        const seconds = album.duration % 60
 
-//       const singerContain = document.getElementById('singer')
-//       singerContain.innerHTML = ` <img
-//                         src="${album.artist.picture}"
-//                         class="rounded-circle me-3"
-//                         width="30"
-//                         height="30"
-//                         alt="singer"
-//                       />
-//                         <div>
-//                         <p class="fs-6 mb-0 d-lg-none">
-//                           ${album.artist.name}
-//                         </p>
-//                         <p class="d-lg-none text-secondary">
-//                           Album &middot; ${album.release_date}
-//                         </p>
-//                         <p class="mb-0 d-none d-lg-block">
-//                          ${album.artist.name} &middot; ${album.release_date} &middot; ${album.nb_tracks}, ${album.duration}.
-//                         </p>
-//                       </div>
-//                       `
-//     })
-//     .catch((error) => {
-//       console.error('Errore:', error)
-//     })
-// }
+        if (hours > 0) {
+          return `${hours}h ${minutes}m ${seconds}s`
+        } else {
+          return `${minutes}m ${seconds}s`
+        }
+      }
 
-// data()
+      album.duration = duration()
+
+      let quantitàBrani = ''
+
+      const braniObrano = function () {
+        if (album.nb_tracks > 1) {
+          quantitàBrani = 'Brani'
+        } else {
+          quantitàBrani = 'Brano'
+        }
+        return quantitàBrani
+      }
+
+      braniObrano()
+
+      const singerContain = document.getElementById('singer-contain')
+      singerContain.innerHTML = `
+                            <div>
+                            <p class="fs-6 mb-0 d-lg-none">
+                              ${album.artist.name}
+                            </p>
+                            <p class="d-lg-none text-secondary">
+                              Album &middot; ${album.release_date}
+                            </p>
+                            <p class="mb-0 d-none d-lg-block">
+                             ${album.artist.name} &middot; ${album.release_date} &middot; ${album.nb_tracks} ${quantitàBrani}, ${album.duration}.
+                            </p>
+                          </div>
+                          `
+      const brani = document.getElementById('brani')
+
+      const titoli = album.tracks.data
+
+      for (let i = 0; i < titoli.length; i++) {
+        brani.innerHTML += ` <div
+                  class="brano-container d-flex justify-content-between align-items-center mt-4 mx-3 mx-lg-5 text-white"
+                >
+                  <div>
+                    <h1 class="fs-4 fw-semibold" style="margin-bottom: 0px">
+                      ${titoli[i].title}
+                    </h1>
+                    <p>${album.artist.name}</p>
+                  </div>
+                  <label class="heart-label mb-3 me-2">
+                    <input type="checkbox" class="heart-checkbox" hidden />
+                    <i class="heart-icon bi bi-suit-heart fs-2"></i>
+                  </label>
+                </div>`
+      }
+    })
+    .catch((error) => {
+      console.error('Errore:', error)
+    })
+}
+
+data()
+
+const colonnaDestra = document.getElementById('colonna-destra')
+const colonnaCentrale = document.getElementById('colonna-centrale')
+const iconX = document.getElementById('icon-x')
+const amici = document.getElementById('amici')
+
+amici.addEventListener('click', () => {
+  colonnaCentrale.classList.remove('col-xl-9')
+  colonnaCentrale.classList.add('col-xl-7')
+  colonnaDestra.classList.remove('d-none')
+  colonnaDestra.classList.add('col-xl-block')
+  amici.classList.remove('d-xl-block')
+})
+
+iconX.addEventListener('click', () => {
+  colonnaCentrale.classList.add('col-xl-9')
+  colonnaCentrale.classList.remove('col-xl-7')
+  colonnaDestra.classList.add('d-none')
+  colonnaDestra.classList.remove('col-xl-block')
+  amici.classList.add('d-xl-block')
+})
